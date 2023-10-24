6c5b14ced6a9f14cc7ad3f4e1060a8e201daf971
FelipeNunezB1 <felipenunezbustamante@gmail.com>
2023-10-24 16:18:07 -0300

    add comocreamos
    se corrige comocreamos:
    
    -cambio de diseño
    - se agrega ¿Quienes somos?

--- src/components/comocreamos.js
+++ src/components/comocreamos.js
@@ -3,17 +3,19 @@ import Divider from '@mui/material/Divider';
 import lanitas from '../assets/images/lanas.png';
 import palitos from '../assets/images/palitos.png';
 import monita from '../assets/images/monita.png';
-import styles from './comocreamos.module.css'
+import "../assets/css/comocreamos.css"
 
 
 const Comocreamos = () => {
     return (
         <div>
             <Divider style={{ height: '2px', marginTop: '60px', marginBottom: '30px' }} />
-            <div className={styles.container1}> {/* Usa los estilos del archivo CSS modular */}
-                <div className={styles.cardTextoIndex}> {/* Usa los estilos del archivo CSS modular */}
-                    <div className={styles.cardBody}> {/* Usa los estilos del archivo CSS modular */}
-                        <div className={styles.tituloGrande}>¿Qué Quiénes Somos?</div> {/* Agrega el título aquí */}
+            <div className="container1">
+                <div className="tituloGrande">
+                    ¿Quiénes Somos?
+                </div>
+                <div class="depth-effect">
+                    <div className="contenido">
                         Momo dreams nace con el deseo de ser tu compañero en las noches más largas y en tus días en los que necesitas un abrazo reconfortante. Cada uno de nuestros amigurumis es tierno, suave y creado con amor, para asegurarnos de que nunca te sientas solo o sola. Te invitamos a descubrir la magia de abrazar a un Momo dreams y encontrar consuelo en su ternura tejida con cariño.
                     </div>
                 </div>
@@ -21,46 +23,25 @@ const Comocreamos = () => {
 
             <Divider style={{ height: '2px', marginTop: '30px', marginBottom: '30px' }} />
 
-            <div className={styles.tituloCardExtension} id="Cejas"> {/* Usa los estilos del archivo CSS modular */}
-                <h2>Proceso de creación de nuestros amigurumis</h2>
+            <div className="tituloGrande">
+                Proceso de un amigurumi
             </div>
-
-            <CenteredImageTextSection
-
-                image={lanitas}
-                text='"Todos nuestros amigurumis son tejidos con diferentes tipos de lanas y las puedes escoger a elección."'
-                textPosition="right"
-            />
-
-            <CenteredImageTextSection
-                image={palitos}
-                text='"Nuestro amigurumis son hechos con los mejores palillos para tener un buen resultado."'
-                textPosition="left"
-            />
-
-            <CenteredImageTextSection
-                image={monita}
-                text='"Cada uno de los amigurumis son creados con mis propias manos, por lo cual cada uno es diferente y tiene un toque especial."'
-                textPosition="right"
-            />
-        </div>
-    );
-};
-
-const CenteredImageTextSection = ({ image, text, textPosition }) => {
-    return (
-        <div className={styles.contenedor}> {/* Usa los estilos del archivo CSS modular */}
-            {textPosition === 'left' && (
-                <div className={styles.texto}> {/* Usa los estilos del archivo CSS modular */}
-                    <p>{text}</p>
+            <div className="contenedor-horizontal"> {/* Utiliza una clase de CSS para mostrar elementos en línea */}
+                <div className="imagen-con-texto">
+                    <img src={lanitas} alt="Imagen" />
+                    <p>'Todos nuestros amigurumis son tejidos con diferentes tipos de lanas y las puedes escoger a elección.'</p>
                 </div>
-            )}
-            <img src={image} alt="Imagen" className={styles.imagen} /> {/* Usa los estilos del archivo CSS modular */}
-            {textPosition === 'right' && (
-                <div className={styles.texto}> {/* Usa los estilos del archivo CSS modular */}
-                    <p>{text}</p>
+                <Divider orientation="vertical" flexItem /> {/* Divider vertical */}
+                <div className="imagen-con-texto">
+                    <img src={palitos} alt="Imagen" />
+                    <p>'Nuestro amigurumis son hechos con los mejores palillos para tener un buen resultado.'</p>
                 </div>
-            )}
+                <Divider orientation="vertical" flexItem /> {/* Divider vertical */}
+                <div className="imagen-con-texto">
+                    <img src={monita} alt="Imagen" />
+                    <p>'Cada uno de los amigurumis son creados con mis propias manos, por lo cual cada uno es diferente y tiene un toque especial.'</p>
+                </div>
+            </div>
         </div>
     );
 };
