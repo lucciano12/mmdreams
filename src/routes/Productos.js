37e9216ba63559e31aedce931568d9c71e7aa024
Lucciano99 <lpradoe@hotmail.com>
2023-10-26 01:06:55 -0300

    26/10
    Continuar con el desarrollo del card

--- src/routes/Productos.js
+++ src/routes/Productos.js
@@ -2,8 +2,22 @@ import React from "react";
 import Navbar from '../components/navbar/Nav';
 import "../assets/css/style.css"
 import MyCarousel from '../components/carrusel';
+import Producto from "../components/Producto/Productos";
 
 
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+//Creacion del Item
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+  
 
 export const Productos = () => {
     return (
@@ -11,10 +25,29 @@ export const Productos = () => {
         <div className="boody1">
             <Navbar />
             <MyCarousel />
-            <h2>Estas en Productos</h2>
+            <h2>Amigurumis</h2>
+
+
+            <Grid container spacing={8}>
+                    <Grid item xs={6}>
+                        <Producto/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Item>xs=4</Item>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Item>xs=4</Item>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Item>xs=8</Item>
+                    </Grid>
+            </Grid>
+
         </div>
 
     );
 }
 
 export default Productos;
+
+
